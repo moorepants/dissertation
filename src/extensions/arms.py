3192ec0e473f47ddc03056fbfc3387b91f4ad432
Jason Moore <moorepants@gmail.com>
2011-12-10 17:49:03 -0800

    Added the locations of the shoulders and grips.

--- src/extensions/arms.py
+++ src/extensions/arms.py
@@ -1,3 +1,5 @@
+from numpy.linalg import norm
+from math import sin, cos
 from scipy import io
 import bicycleparameters as bp
 from dtk import bicycle
@@ -37,6 +39,12 @@ par['l4'] = h.B1.relCOM[2][0]
 IG = h.B1.relInertia
 par['ig11'] = IG[0, 0]
 par['ig33'] = IG[2, 2]
+par['d6'] = (h.B1.pos[0, 0] * cos(benchmark['lam']) - h.B1.pos[2, 0] * sin(benchmark['lam']) -
+        benchmark['rR'] * sin(benchmark['lam']))
+par['d7'] = abs(h.B1.pos[1, 0])
+par['d8'] = (h.B1.pos[0, 0] * sin(benchmark['lam']) + h.B1.pos[2, 0] * cos(benchmark['lam']) +
+        benchmark['rR'] * cos(benchmark['lam']))
+par['d12'] = h.B1.length
 
 # right lower arm, H
 par['mh'] = h.B2.Mass
@@ -44,6 +52,14 @@ par['l5'] = h.B2.relCOM[2][0]
 IH = h.B2.relInertia
 par['ih11'] = IH[0, 0]
 par['ih33'] = IH[2, 2]
+knuckle = h.b[6].pos
+par['d11'] = ((knuckle[2, 0] + benchmark['rF']) * cos(benchmark['lam']) +
+        (knuckle[0, 0] - benchmark['w'])
+            * sin(benchmark['lam']))
+par['d9'] = ((knuckle[0, 0] - benchmark['w'] - par['d11'] * sin(benchmark['lam'])) /
+            cos(benchmark['lam']))
+par['d10'] = abs(knuckle[1, 0])
+par['d13'] = norm(knuckle - h.B2.pos)
 
 # left upper arm, I
 par['mi'] = h.A1.Mass

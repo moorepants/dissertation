e0542891791403c136788a6a690aadb4d5877a13
Jason Moore <moorepants@gmail.com>
2011-12-14 18:25:58 -0800

    Improved gyro bike plots.

--- src/extensions/gyro/gyrobike.py
+++ src/extensions/gyro/gyrobike.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python
 
-# built in imports
-import sys
-
 # dependencies
-import numpy as np
 import bicycleparameters as bp
 from dtk import bicycle
 
 # local dependencies
-sys.path.append('/media/Data/Documents/School/UC Davis/Bicycle Mechanics/bicycle/alparse')
-from models.GyroBike.GyroBike import GyroBike, LinearGyroBike
+from GyroBike import GyroBike
 
 # create the Whipple model (with my parameters)
 gyroNonLinear = GyroBike()
 
 # load the benchmark parameters
 pathToData='/media/Data/Documents/School/UC Davis/Bicycle Mechanics/BicycleParameters/data/'
-benchmark = bp.Bicycle('Benchmark', pathToData)
-benchmarkPar = bp.io.remove_uncertainties(benchmark.parameters['Benchmark'])
+gyro = bp.Bicycle('Gyro', pathToData, forceRawCalc=True)
+gyro.add_rider('Child', reCalc=True)
+benchmarkPar = bp.io.remove_uncertainties(gyro.parameters['Benchmark'])
 # convert to my parameter set
 moorePar = bicycle.benchmark_to_moore(benchmarkPar, oldMassCenter=False)
-moorePar['mg'] = moorePar['mf']
-moorePar['ig11'] = moorePar['if11']
-moorePar['ig22'] = moorePar['if22']
+moorePar['mg'] = benchmarkPar['mD']
+moorePar['ig11'] = benchmarkPar['IDxx']
+moorePar['ig22'] = benchmarkPar['IDyy']
 gyroNonLinear.set_parameters(moorePar)
 
 # set the initial conditions to match Meijaard2007
-speedNaught = 0.5 #4.6
+speedNaught = 4.6
 rollRateNaught = 0.5
-pitchAngle = bicycle.pitch_from_roll_and_steer(0., 0., moorePar['rF'], moorePar['rR'],
+pitchAngle = bicycle.pitch_from_roll_and_steer(0., 0., moorePar['rf'], moorePar['rr'],
         moorePar['d1'], moorePar['d2'], moorePar['d3'])
-wheelAngSpeed = -speedNaught / moorePar['rR']
+wheelAngSpeed = -speedNaught / moorePar['rr']
 gyroNonLinear.initialConditions[gyroNonLinear.stateNames.index('q5')] = pitchAngle
 gyroNonLinear.initialConditions[gyroNonLinear.stateNames.index('u4')] = rollRateNaught
 gyroNonLinear.initialConditions[gyroNonLinear.stateNames.index('u6')] = wheelAngSpeed
-gyroNonLinear.initialConditions[gyroNonLinear.stateNames.index('u9')] = 0.5 * wheelAngSpeed
+gyroNonLinear.initialConditions[gyroNonLinear.stateNames.index('u9')] = 4 * wheelAngSpeed
+print 4 * wheelAngSpeed
 
 # integration settings
 ts = 0.01 # step size
@@ -47,16 +44,8 @@ gyroNonLinear.simulate()
 
 # plot figure 4 from Meijaard2007 using my model
 rollRate = gyroNonLinear.simResults['y'][:, gyroNonLinear.outputNames.index('u4')]
-speed = -gyroNonLinear.simResults['y'][:, gyroNonLinear.outputNames.index('u6')] * gyroNonLinear.parameters['rR']
-steerRate = gyroNonLinear.simResults['y'][:, gyroNonLinear.outputNames.index('u7')]
+speed = -gyroNonLinear.simResults['y'][:, gyroNonLinear.outputNames.index('u6')] * gyroNonLinear.parameters['rr']
+steerrate = gyroNonLinear.simResults['y'][:, gyroNonLinear.outputNames.index('u7')]
 time = gyroNonLinear.simResults['t']
-newFig = bicycle.meijaard_figure_four(time, rollRate, steerRate, speed)
-newFig.savefig('../../figures/extensions/gyro-nonlin-sim.png', dpi=300)
-
-#
-equilibrium = np.zeros(len(gyroNonLinear.stateNames))
-equilibrium[gyroNonLinear.stateNames.index('q5')] = pitchAngle
-equilibrium[gyroNonLinear.stateNames.index('u6')] = wheelAngSpeed
-#equilibrium[gyroNonLinear.stateNames.index('u9')] = 0.5 * wheelAngSpeed
-gyroLin = LinearGyroBike()
-gyroLin.linear(equilibrium)
+newFig = bicycle.meijaard_figure_four(time, rollRate, steerrate, speed)
+newFig.savefig('../../../figures/extensions/gyro-nonlin-sim.png', dpi=300)
--- src/extensions/gyro/gyrobike_linear.py
+++ src/extensions/gyro/gyrobike_linear.py
@@ -9,61 +9,77 @@ import bicycleparameters as bp
 from dtk import bicycle
 
 # local dependencies
-sys.path.append('/media/Data/Documents/School/UC Davis/Bicycle Mechanics/AutolevToolKit')
-from models.GyroBike.GyroBike import LinearGyroBike
+from GyroBike import LinearGyroBike
 
 # create the linear gyrobike model
 gyrobike = LinearGyroBike()
 
-# load the benchmark parameters
+# create the gyro bike
 pathToData='/media/Data/Documents/School/UC Davis/Bicycle Mechanics/BicycleParameters/data/'
-gyro = bp.Bicycle('Gyro', pathToData)
+gyro = bp.Bicycle('Gyro', pathToData, forceRawCalc=True)
+
+# set the model parameters
 benchmarkPar = bp.io.remove_uncertainties(gyro.parameters['Benchmark'])
-# convert to my parameter set
-moorePar = bicycle.benchmark_to_moore(benchmarkPar, oldMassCenter=False)
-moorePar['mg'] = benchmarkPar['mF']
-moorePar['ig11'] = benchmarkPar['IFxx']
-moorePar['ig22'] = benchmarkPar['IFyy']
+moorePar = bicycle.benchmark_to_moore(benchmarkPar)
+moorePar['mg'] = benchmarkPar['mD']
+moorePar['ig11'] = benchmarkPar['IDxx']
+moorePar['ig22'] = benchmarkPar['IDyy']
 gyrobike.set_parameters(moorePar)
 
-# set the equilibrium point
-speedNaught = 0.5
-pitchAngle = bicycle.pitch_from_roll_and_steer(0., 0., moorePar['rF'], moorePar['rR'],
-        moorePar['d1'], moorePar['d2'], moorePar['d3'])
-wheelAngSpeed = -speedNaught / moorePar['rR']
+# set the default equilibrium point
+speedNaught = 0.5 # just over 1 mph, really slow walking
+pitchAngle = bicycle.pitch_from_roll_and_steer(0., 0., moorePar['rf'],
+        moorePar['rr'], moorePar['d1'], moorePar['d2'], moorePar['d3'])
+wheelAngSpeed = -speedNaught / moorePar['rr']
 equilibrium = np.zeros(len(gyrobike.stateNames))
 equilibrium[gyrobike.stateNames.index('q5')] = pitchAngle
 equilibrium[gyrobike.stateNames.index('u6')] = wheelAngSpeed
 
-equilibrium[gyrobike.stateNames.index('u9')] = 0.
-gyrobike.linear(equilibrium)
-start = 0.
-stop = -10.0 / moorePar['rR']
-off = gyrobike.plot_root_loci('u6', start, stop, num=50, axes='parameter',
-        parts='real')
-off.savefig('../../figures/extensions/gyrobike-flywheel-off.png')
+figDir = '../../../figures/extensions/'
 
-equilibrium[gyrobike.stateNames.index('u9')] = -50.0
-gyrobike.linear(equilibrium)
-start = 0.
-stop = -10.0 / moorePar['rR']
-med = gyrobike.plot_root_loci('u6', start, stop, num=50, axes='parameter',
-        parts='real')
-med.savefig('../../figures/extensions/gyrobike-flywheel-medium.png')
+settings = {'num':100,
+            'axes':'parameter',
+            'pub':True,
+            'ylim':(-10, 10)}
 
-equilibrium[gyrobike.stateNames.index('u9')] = -100.0
+# generate a root loci with respect to speed for a baseline
 gyrobike.linear(equilibrium)
 start = 0.
-stop = -10.0 / moorePar['rR']
-fast = gyrobike.plot_root_loci('u6', start, stop, num=50, axes='parameter',
-        parts='real')
-fast.savefig('../../figures/extensions/gyrobike-flywheel-fast.png')
+stop = -10.0 / moorePar['rr']
+flywheelOff = gyrobike.plot_root_loci('u6', start, stop, factor=('v',
+    -moorePar['rr']), units='m/s', **settings)
+flywheelOff.savefig(figDir + 'gyrobike-flywheel-off.png')
 
-#
-equilibrium[gyrobike.stateNames.index('u6')] = wheelAngSpeed
+# now generate a root loci with respect to flywheel speed
 gyrobike.linear(equilibrium)
 start = 0.
-stop = -300.
-fast = gyrobike.plot_root_loci('u9', start, stop, num=50, axes='parameter',
-        parts='real')
-fast.savefig('../../figures/extensions/gyrobike-vary-flywheel.png')
+stop = -10000. / 60. * 2 * np.pi
+flywheelOn = gyrobike.plot_root_loci('u9', start, stop, factor=('$u_9$', -60. /
+    2 / np.pi), units='rpm', **settings)
+flywheelOn.savefig(figDir + 'gyrobike-vary-flywheel.png')
+
+# create the same graphs but with a rider
+# warning, as of now the arms are not properly set for this bicycle
+gyro.add_rider('Child')
+benchmarkPar = bp.io.remove_uncertainties(gyro.parameters['Benchmark'])
+moorePar = bicycle.benchmark_to_moore(benchmarkPar)
+moorePar['mg'] = benchmarkPar['mD']
+moorePar['ig11'] = benchmarkPar['IDxx']
+moorePar['ig22'] = benchmarkPar['IDyy']
+gyrobike.set_parameters(moorePar)
+
+# generate a root loci with respect to speed for a baseline
+gyrobike.linear(equilibrium)
+start = 0.
+stop = -10.0 / moorePar['rr']
+flywheelOffRider = gyrobike.plot_root_loci('u6', start, stop, factor=('v',
+    -moorePar['rr']), units='m/s', **settings)
+flywheelOffRider.savefig(figDir + 'gyrobike-flywheel-off-rider.png')
+
+# now generate a root loci with respect to flywheel speed
+gyrobike.linear(equilibrium)
+start = 0.
+stop = -10000. / 60. * 2 * np.pi
+flywheelOnRider = gyrobike.plot_root_loci('u9', start, stop, factor=('$u_9$', -60. /
+    2 / np.pi), units='rpm',  **settings)
+flywheelOnRider.savefig(figDir + 'gyrobike-vary-flywheel-rider.png')

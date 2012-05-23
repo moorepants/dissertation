c3517ce6511c9dd6ed36c154386a066879b82081
Jason Moore <moorepants@gmail.com>
2012-05-23 09:37:01 -0700

    Added pdf output to phasor diagrams.

--- src/eom/plot_eigenvectors.py
+++ src/eom/plot_eigenvectors.py
@@ -8,6 +8,16 @@ import matplotlib.pyplot as plt
 import bicycleparameters as bp
 from dtk import bicycle
 
+# create the Whipple model (with my parameters)
+try:
+    f = open('Whipple.py', 'r')
+except IOError:
+    from altk import alparse
+    alparse.alparse('Whipple', 'Whipple', code='Python')
+else:
+    f.close()
+    del f
+
 # local dependencies
 from Whipple import LinearWhipple
 
@@ -24,24 +34,14 @@ params = {'backend': 'ps',
           'figure.dpi': 200}
 plt.rcParams.update(params)
 
-# create the Whipple model (with my parameters)
-try:
-    f = open('Whipple.py', 'r')
-except IOError:
-    from altk import alparse
-    alparse.alparse('Whipple', 'Whipple', code='Python')
-else:
-    f.close()
-    del f
-
-whip = LinearWhipple()
-
 # load the benchmark parameters
 pathToData='/media/Data/Documents/School/UC Davis/Bicycle Mechanics/BicycleParameters/data/'
 benchmark = bp.Bicycle('Benchmark', pathToData)
 benchmarkPar = bp.io.remove_uncertainties(benchmark.parameters['Benchmark'])
 # convert to my parameter set
 moorePar = bicycle.benchmark_to_moore(benchmarkPar, oldMassCenter=False)
+
+whip = LinearWhipple()
 whip.set_parameters(moorePar)
 
 # linearize about the nominal configuration
@@ -64,8 +64,10 @@ for i, f in enumerate(figs):
         text.set_text('$' + text.get_text()[0] + '_' + text.get_text()[1] + '$')
     speed = 'Half'
     f.savefig('../../figures/eom/eVec' + speed + '-' + str(i + 1) + '.png')
+    f.savefig('../../figures/eom/eVec' + speed + '-' + str(i + 1) + '.pdf')
 figPath = '../../figures/eom/'
 os.system('montage -geometry +0+0 {0}eVecHalf-1.png {0}eVecHalf-2.png {0}eVecHalf-3.png {0}eVecHalf-4.png {0}eVecHalf.png'.format(figPath))
+#os.system('montage -geometry +0+0 {0}eVecHalf-1.pdf {0}eVecHalf-2.pdf {0}eVecHalf-3.pdf {0}eVecHalf-4.pdf {0}eVecHalf.pdf'.format(figPath))
 
 fig_size =  [2., 2.]
 params = {'figure.figsize': fig_size}
@@ -88,7 +90,9 @@ for i, f in enumerate(figs):
     bbox = ax.get_position()
     ax.set_position([0.125, 0.1, 0.9 - 0.125, 0.9 - 0.2])
     f.savefig('../../figures/eom/eVec' + speed + '-' + str(i + 1) + '.png')
+    f.savefig('../../figures/eom/eVec' + speed + '-' + str(i + 1) + '.pdf')
 os.system('montage -geometry +0+0 {0}eVecThree-1.png {0}eVecThree-2.png {0}eVecThree-3.png {0}eVecThree.png'.format(figPath))
+#os.system('montage -geometry +0+0 {0}eVecThree-1.pdf {0}eVecThree-2.pdf {0}eVecThree-3.pdf {0}eVecThree.pdf'.format(figPath))
 
 # stable
 speedNaught = 5.0
@@ -107,7 +111,9 @@ for i, f in enumerate(figs):
     bbox = ax.get_position()
     ax.set_position([0.125, 0.1, 0.9 - 0.125, 0.9 - 0.2])
     f.savefig('../../figures/eom/eVec' + speed + '-' + str(i + 1) + '.png')
+    f.savefig('../../figures/eom/eVec' + speed + '-' + str(i + 1) + '.pdf')
 os.system('montage -geometry +0+0 {0}eVecFive-1.png {0}eVecFive-2.png {0}eVecFive-3.png {0}eVecFive.png'.format(figPath))
+#os.system('montage -geometry +0+0 {0}eVecFive-1.pdf {0}eVecFive-2.pdf {0}eVecFive-3.pdf {0}eVecFive.pdf'.format(figPath))
 
 # unstable capsize
 speedNaught = 8.0
@@ -126,4 +132,6 @@ for i, f in enumerate(figs):
     bbox = ax.get_position()
     ax.set_position([0.125, 0.1, 0.9 - 0.125, 0.9 - 0.2])
     f.savefig('../../figures/eom/eVec' + speed + '-' + str(i + 1) + '.png')
+    f.savefig('../../figures/eom/eVec' + speed + '-' + str(i + 1) + '.pdf')
 os.system('montage -geometry +0+0 {0}eVecSeven-1.png {0}eVecSeven-2.png {0}eVecSeven-3.png {0}eVecSeven.png'.format(figPath))
+#os.system('montage -geometry +0+0 {0}eVecSeven-1.pdf {0}eVecSeven-2.pdf {0}eVecSeven-3.pdf {0}eVecSeven.pdf'.format(figPath))
